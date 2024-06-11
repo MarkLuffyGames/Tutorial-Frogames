17e01cb56b968beb4188f6fd7fa3963cbd7527a0
MarkLuffyGames <marcosluffy97@gmail.com>
2024-06-10 23:32:10 -0300

    Challenge 5 Completed

--- QuickClick/Assets/Challenge 5/Scripts/TargetX.cs
+++ QuickClick/Assets/Challenge 5/Scripts/TargetX.cs
@@ -27,7 +27,7 @@ public class TargetX : MonoBehaviour
     }
 
     // When target is clicked, destroy it, update score, and generate explosion
-    private void OnMouseEnter()
+    private void OnMouseDown()
     {
         if (gameManagerX.isGameActive)
         {
@@ -75,7 +75,7 @@ public class TargetX : MonoBehaviour
     }
 
     // After a delay, Moves the object behind background so it collides with the Sensor object
-    IEnumerator RemoveObjectRoutine ()
+    IEnumerator RemoveObjectRoutine()
     {
         yield return new WaitForSeconds(timeOnScreen);
         if (gameManagerX.isGameActive)
